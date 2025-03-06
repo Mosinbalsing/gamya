4d33f970d7e8a9c425e6dd755b955a69d82eba7c
Mosinbalsing <mosinbalsing@gmail.com>
2025-03-06 19:34:21 +0530

    path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import MainLayout from '@/layouts/MainLayout'
-import Home from './pages/Home'
+import Home from '@/Pages/Home'
 import ProductPage from '@/Pages/ProductPage'
 import  Navbar  from '@/components/Navbar'
 
